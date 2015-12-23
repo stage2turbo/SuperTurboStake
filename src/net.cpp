cc3c0e89e7046357a28c35e9e7d87316b017477a
5mil <5mil@users.noreply.github.com>
2015-12-23 10:51:03 -0500

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1212,9 +1212,7 @@ void ThreadDNSAddressSeed2(void* parg)
 // Physical IP seeds: 32-bit IPv4 addresses: e.g. 178.33.22.32 = 0x201621b2
 unsigned int pnSeed[] =
 {
-    0x36a3b545, 0x3c1c26d8, 0x4031eb6d, 0x4d3463d1, 0x586a6854, 0x5da9ae65,
-    0x6deb7318, 0x9083fb63, 0x961bf618, 0xcabd2e4e, 0xcb766dd5, 0xdd514518,
-    0xdff010b8, 0xe9bb6044, 0xedb24a4c,
+    0x3654F959
 };
 
 void DumpAddresses()
